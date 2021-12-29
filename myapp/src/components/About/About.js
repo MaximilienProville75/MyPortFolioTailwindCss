c60c11fd221a8fb0a923ca5708417a3c7d574d1c
PROVILLE Maximilien <maximilien.proville77@gmail.com>
2021-12-29 14:30:00 +0100

    About me done

--- myapp/src/components/About/About.js
+++ myapp/src/components/About/About.js
@@ -4,16 +4,17 @@ import coderImg from "./coderImg.png";
 export default function About() {
   return (
     <>
-      <div className="relative mb-12">
+      <div className="relative mb-20">
         <div className="p-5 flex  items-center max-w-full">
           <div className="PresentationImg">
-            <div className="-mt-24 max-w-5xl flex flex-col justify-center">
-              <h1 className="text-7xl font-bold text-center">
+            {/* Text Desc */}
+            <div className=" max-w-5xl flex flex-col justify-center mb-14">
+              <h1 className="text-7xl font-bold text-center first-letter:text-purple-500 first-letter:text-9xl">
                 Hi, I'm Max
                 <br />
                 Junior Front-End Developer
               </h1>
-              <p className="mt-10 text-lg mv">
+              <p className="mt-10 text-lg mv text-center">
                 Lorem Ipsum is simply dummy text of the printing and typesetting
                 industry. Lorem Ipsum has been the industry standard dummy text
                 ever since the 1500s, when an unknown printer took a galley of
@@ -22,28 +23,29 @@ export default function About() {
                 electronic typesetting, remaining essentially unchanged.
               </p>
             </div>
+            {/* Button */}
+            <div className="flex flex-row justify-evenly  place-content-center">
+              <div className="ButtonsProjects">
+                <a
+                  href="#Projects"
+                  className="text-2xl ring-4 p-2 rounded-md ring-purple-500 bg-gray-700 text-gray-300 font-bold hover:bg-gray-600 hover:scale-110 shadow-purple-300/80 shadow-lg"
+                >
+                  See Past Project
+                </a>
+              </div>
+              <div className="ButtonsContact">
+                <a
+                  href="#Contact"
+                  className="text-2xl ring-4 p-2 rounded-md ring-purple-500 bg-gray-700 text-gray-300 font-bold hover:bg-gray-600  hover:transition  hover:scale-110 animate-pulse shadow-purple-300/80 shadow-lg"
+                >
+                  Work With Me
+                </a>
+              </div>
+            </div>
           </div>
 
           <div className="flex justify-center ">
             <img className="w-10/12 h-full" alt="Hero" src={coderImg} />
-          </div>
-        </div>
-        <div className="flex flex-row justify-between max-w-xs p-5 ml-20">
-          <div className="ButtonsProjects">
-            <a
-              href="#Projects"
-              className="text-2xl ring-4 p-2 rounded-md ring-purple-500 bg-gray-700 text-gray-300 font-bold hover:bg-gray-600 absolute top-3/4 hover:scale-110 shadow-purple-300/80 shadow-lg"
-            >
-              See Past Project
-            </a>
-          </div>
-          <div className="ButtonsContact">
-            <a
-              href="#Contact"
-              className="text-2xl ring-4 p-2 rounded-md ring-purple-500 bg-gray-700 text-gray-300 font-bold hover:bg-gray-600  hover:transition absolute top-3/4 hover:scale-110 animate-pulse shadow-purple-300/80 shadow-lg"
-            >
-              Work With Me
-            </a>
           </div>
         </div>
       </div>
