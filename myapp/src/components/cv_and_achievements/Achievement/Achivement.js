b51e243ef3ccc9e5725fcda90d015c34fc023204
PROVILLE Maximilien <maximilien.proville77@gmail.com>
2022-01-02 19:15:28 +0100

    Addond CV Achivement page begin

---
+++ myapp/src/components/cv_and_achievements/Achievement/Achivement.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export class Achivements extends React.Component {
+  render() {
+    return;
+  }
+}
---
+++ myapp/src/components/cv_and_achievements/MainCvAch.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Achivements } from "./Achievement/Achivement";
+import { Cv } from "./Cv/Cv";
+
+export class MainCvAch extends React.Component {
+  render() {
+    return;
+  }
+}
