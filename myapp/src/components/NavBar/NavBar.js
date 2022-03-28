779291366454923d97589bec4115f043e61852d4
PROVILLE Maximilien <maximilien.proville77@gmail.com>
2022-03-28 22:58:52 +0200

    28-03 Design Responsive fixed

--- myapp/src/components/NavBar/NavBar.js
+++ myapp/src/components/NavBar/NavBar.js
@@ -8,7 +8,7 @@ export default function Navbar() {
   return (
     <div className="bg-gray-800 sticky top-0 z-10 ">
       <div className="Logo"></div>
-      <div className="flex flex-row p-5 justify-between text-2xl font-bold sm:text-sm sm:p-3">
+      <div className="flex flex-row p-5 justify-between text-2xl font-bold sm:text-xs sm:p-3">
         <nav className=" flex justify-between w-3/12 sm:w-auto sm:gap-2">
           <a className="text-white font-extrabold ">PROVILLE Maximilien</a>
           <a className="hover:text-white " href="#Projects">
@@ -18,11 +18,10 @@ export default function Navbar() {
             Skills
           </a>
         </nav>
-        <div className=" flex flex-row gap-10">
-          <div>
-            <LangToggle />
-            <Toggle />
-          </div>
+        <div className=" flex flex-row gap-10 sm:gap-1 \">
+          <LangToggle />
+          <Toggle />
+
           <a href="#Contact" className="hover:text-purple-500 sm:text-white">
             Hire-Me <FontAwesomeIcon icon={faArrowRight} />
           </a>
